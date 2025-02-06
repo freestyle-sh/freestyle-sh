54b7fdb0a3ae2fb1c23810c30ffc5ff8b7e10faf
Jacob Zwang <59858341+JacobZwang@users.noreply.github.com>
2025-02-06 12:55:43 -0800

    get logs for most recent web deployment (untested) and update sandbox package

--- src/commands/logs.js
+++ src/commands/logs.js
@@ -2,18 +2,21 @@ import { FreestyleSandboxes } from "freestyle-sandboxes";
 import { createCommand } from "commander";
 
 export const logsCommand = createCommand("logs")
-  .option("--domain <domain>", "Domain of deployment")
+  // todo: add this back when freestyle supports getting logs by domain
+  // .option("--domain <domain>", "Domain of deployment")
   .action(async () => {
     const api = new FreestyleSandboxes({
       apiKey: process.env.FREESTYLE_API_KEY,
       baseUrl: process.env.FREESTYLE_API_URL,
     });
 
-    await api
-      .getLogs({
-        domains: [logsCommand.opts().domain],
-      })
-      .then((logs) => {
-        console.log(logs);
-      });
+    const deployments = await api.listWebDeployments().then((deploy) => {
+      deploy.entries;
+    });
+
+    let mostRecentId = deploy.entries.at(0).deploymentId;
+
+    await api.getLogs(mostRecentId).then((logs) => {
+      console.log(logs);
+    });
   });
